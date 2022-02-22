0c012e089021f71f1570b22b84b595ecbc9b949a
Ankur-function <ankurraj834062@gmail.com>
2022-02-22 19:05:52 +0530

    again did modificatios

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -1,11 +1,4 @@
-let url = "https://www.google.com"
-
-
-function printMyMessage(message){
-    console.log(message)
+function welcome() {
+    console.log("Welcome to my application. I am Ankur Raj and a part of Functionup")
 }
-
-
-module.exports.endpoint = url
-
-module.exports.printMessage = printMyMessage
+module.exports.welcome11 = welcome;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,21 @@
+const welcome = require("../logger")
+const mydetails = require("..util/helper")
+const format = require("../validator/Formatter")
 const express = require('express');
+
+
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    welcome.welcome11();
+    mydetails.date();
+    mydetails.month();
+    mydetails.batchinfo();
+    format.Trim("   functionup   ");
+    format.lowerCase("functionup");
+    format.upperCase("FuNctionUp");
+    
     res.send('My first ever api!')
 });
 
