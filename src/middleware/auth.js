451bf89048788b0d5c1bd64b344e8fe430962e91
Ankur-function <ankurraj834062@gmail.com>
2022-03-10 10:19:29 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,32 @@
-const authenticate = function(req, req, next) {
+const jwt=require("jsonwebtoken")
+
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
 
-    next()
+    let token = req.headers["x-Auth-token"];
+
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  let decodedToken = jwt.verify(token, "functionup-thorium");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
+
+next()
 }
+module.exports.authenticate=authenticate
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+   //userId for which the request is made. In this case message to be posted.
+    let userToBeModified = req.params.userId
+  //userId for the logged-in user
+    let userLoggedIn = decodedToken.userId
+
+//userId comparision to check if the logged-in user is requesting for their own data
+if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+next()
 }
+module.exports.authorise=authorise
